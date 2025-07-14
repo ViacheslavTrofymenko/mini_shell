7d47dea245a5d49d02675e527a3e95c8828658fc
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-07-14 20:39:50 +0200

    Added crit_except after execv

--- src/execute.c
+++ src/execute.c
@@ -6,15 +6,15 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 23:04:08 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/07/14 14:43:22 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/07/14 20:18:29 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void	apply_redirs(t_cmd *cmd);
-static void	exec_or_exit(t_cmd *cmd);
-static void	exec_one_cmd(t_cmd *cmd);
+static void	exec_or_exit(t_shell *shell, int index);
+static void	exec_one_cmd(t_shell *shell);
 static void	exec_pipe_cmds(t_shell *shell);
 int	ft_error(int num, char *str);
 char	*ft_check_command(char **path_list, char *cmd);
@@ -24,7 +24,7 @@ char	*ft_get_path_command(char **cmd, char **envp);
 void	execute_cmds(t_shell *shell)
 {
 	if (shell->num_cmds == 1)
-		exec_one_cmd(&shell->cmds[0]);
+		exec_one_cmd(shell);
 	else
 		exec_pipe_cmds(shell);
 }
@@ -56,10 +56,12 @@ static void	apply_redirs(t_cmd *cmd)
 		close(fd);
 	}
 }
-static void	exec_or_exit(t_cmd *cmd)
+static void	exec_or_exit(t_shell *shell, int i)
 {
-	char *path_cmd;
+	char	*path_cmd;
+	t_cmd	*cmd;
 
+	cmd = &shell->cmds[i];
 	apply_redirs(cmd);
 	path_cmd = ft_get_path_command(cmd->args, cmd->envp);
 	if (path_cmd)
@@ -68,16 +70,16 @@ static void	exec_or_exit(t_cmd *cmd)
 		free(path_cmd);
 	}
 	ft_error(2, cmd->args[0]);
-	exit(127);
+	crit_except(shell, 127);
 }
 
-static void	exec_one_cmd(t_cmd *cmd)
+static void	exec_one_cmd(t_shell *shell)
 {
 	pid_t pid;
 
 	pid = fork();
 	if (pid == 0)
-		exec_or_exit(cmd);
+		exec_or_exit(shell, 0);
 	else if (pid > 0)
 		waitpid(pid, NULL, 0);
 	else
@@ -110,7 +112,7 @@ static void	exec_pipe_cmds(t_shell *shell)
 				dup2(pipe_fd[1], STDOUT_FILENO);
 				close(pipe_fd[1]);
 			}
-			exec_or_exit(&shell->cmds[i]);
+			exec_or_exit(shell, i);
 		}
 		if (prev_fd != -1)
 			close(prev_fd);
